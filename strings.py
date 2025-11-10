0cdead4a22dd85b52167cb8982939454f9104498
Christian <71664957+Christian-Mar@users.noreply.github.com>
2025-11-10 21:51:53 +0100

    strings

---
+++ strings.py
@@ -0,0 +1,35 @@
+import uuid
+
+fruit = "banana"
+
+first_letter = fruit[0]
+second_letter = fruit[1]
+
+i = -1
+print(fruit[i])
+print(fruit[0:3])
+print(fruit[0:10])
+print(fruit[0:5:2])
+print(fruit[3:])
+print(fruit[::-1])
+print("20251101" > "20251010")
+# Datums altijd scrhijven als 2025-10-1
+# In toepassingen altijd calender controll gebruiken om inputfouten te voorkomen
+
+print(uuid.uuid4())
+# beter:
+print(uuid.uuid5())
+# want sorteerbaar ??? -> uuid7 - Python 3.14?
+
+ip = ["127", "0", "0", "1"]
+print(".".join(ip))
+
+# files:
+# R - read
+# W - write
+# A - append
+# RB - read binary ...
+
+name = "Yves"
+if name is not None: # niet !=
+    pass
