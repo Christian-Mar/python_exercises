fe8b9f50c85f75c5406867ff9a2d6656101e015c
Christian <71664957+Christian-Mar@users.noreply.github.com>
2025-11-22 11:02:51 +0100

    count words and letters

--- count_words.py
+++ count_words.py
@@ -6,8 +6,24 @@ with open("words.txt", 'r') as f:
                 word = line.strip()
                 total = total + 1
                 for letter in word: 
-                    if letter == "E" or letter == "e":
+                    if letter == "Y" or letter == "y":
                         count = count + 1
         print(total)
+        print(f"{count} is the total amount of y's in words.txt")       
+ 
+# The count of Y/y is an inner loop, so it counts all Y/y's
+
+total = 0
+count = 0
+
+with open("words.txt", 'r') as f:
+        for line in f:
+                word = line.strip()
+                total = total + 1
+                if 'y' in word.lower(): 
+                    count = count + 1
+        print(total)
         print(count)       
-        print(f"{round(count / total * 100)}% has an 'e' in it")    
+        print(f"{round(count / total * 100)}% or {count} words have at least one 'y' in it")    
+
+# The if-functionality stays in the same for-loop
