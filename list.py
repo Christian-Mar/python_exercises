e97defda5d6801f7be35b8b85a0978e8ebd2b0a5
Christian <71664957+Christian-Mar@users.noreply.github.com>
2025-11-24 21:58:24 +0100

    list_ex

---
+++ list.py
@@ -0,0 +1,21 @@
+CLASSROOM = ['Yves', 'Brent', 'Femke', 'Jeroen', 'Uwe', 'Bas', 'Robin', 'Hendrik-Jan', 'Christian']
+CLASSROOM_NEW = []
+
+PROVINCES = ['West-Vlaanderen', 'Oost-Vlaanderen', 'Antwerpen', 'Vlaams-Brabant', 'Waals-Brabant', 'Limburg', 'Namen', 'Luxemburg']
+PROVINCES_NEW = []
+
+def clear_names_starting_with_vowel(first_list, new_list):
+    vowels = 'AEIOUY'
+    for name in first_list:
+        if name[0] not in vowels:
+            new_list.append(name)
+    return new_list
+
+clear_names_starting_with_vowel(CLASSROOM, CLASSROOM_NEW)
+print("Original:", CLASSROOM)
+print("Filtered:", CLASSROOM_NEW)
+
+clear_names_starting_with_vowel(PROVINCES, PROVINCES_NEW)
+print("Original:", PROVINCES)
+print("Filtered:", PROVINCES_NEW)
+
